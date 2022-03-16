e6ff1d190d4983a71a7e206a777b6cb99b3f269a
DogBoneZone <94696731+DogBoneZone@users.noreply.github.com>
2022-03-16 15:29:35 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,85 @@
+const Discord = require("discord.js")
+let logger = require('winston')
+let auth = require('./auth.json')
+const responses = require('./responses.json')
+
+// Configure Logger Settings
+logger.remove(logger.transports.Console)
+logger.add(new logger.transports.Console, {
+    colorize: true
+})
+logger.level = "debug"
+
+// Initialize Bot
+const bot = new Discord.Client({
+    token: auth.token,
+    autorun: true,
+    intents: ['GUILDS', 'GUILD_MESSAGES']
+})
+0
+bot.once('ready', function (evt) {
+    logger.info('Connected')
+    logger.info('Annoying Kid has Arrived.')
+})
+
+bot.on('message', message => {
+    console.log(message)
+
+    if (message.content.substring(0, 1) === '!') {
+        let stringArray = message.content.substring(1).split(' ')
+        let cmd = stringArray[0]
+
+        switch (cmd) {
+            case 'help':
+                message.channel.send(
+                    `
+Ugh, I can't believe you don't even know my commands yet, jfc. Just look below and see what I have to offer.
+
+Prepend commands with '!' to execute the following commands:
+- **!help**: You're here already.
+- **!insult**: Receive an insult from me. 
+- **!wiki [thing you want to search for]**: I pull up a wikipedia page for your lazy ass.
+- **!alex**: Bitch ass Alex
+- **!jerry**: Bitch ass Jerry
+- **!gabe**: Bitch ass Gabe
+- **!slug**: Pimp Nick
+                    `
+                )
+                break
+
+            case 'insult':
+                message.channel.send(responses.insult[Math.floor(Math.random() * responses.insult.length)])
+                break
+
+            case 'wiki':
+                for (let string of stringArray) {
+                    string.split('')[0].toUpperCase()
+                }
+                stringArray.shift()
+                stringArray.join('_')
+                message.channel.send(`
+                    ${responses.deliver[Math.floor(Math.random() * responses.deliver.length)]}
+                    https://en.wikipedia.org/wiki/${stringArray}`
+                )
+                break
+
+            case 'alex':
+                message.channel.send({files: ['images/alex_sombrero.png']})
+                break
+
+            case 'jerry':
+                message.channel.send({files: ['images/jerry_optimistic.png']})
+                break
+
+            case 'gabe':
+                message.channel.send({files: ['images/gabe_sinister.png']})
+                break
+
+            case 'slug':
+                message.channel.send({files: ['images/slug_doubtful.png']})
+                break
+        }
+    }
+})
+
+bot.login('OTUzNzI3NTc5ODU1MTU5Mzk3.YjIyBg.AJQ1VBYtzQ763kED3qq9VFdWwg0')
